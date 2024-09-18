800e9928e4e98e97ef0756916ab621a7c8ac5584
XaerickSReborn <u202213358@upc.edu.pe>
2024-09-17 21:17:01 -0500

    feat(courses): add course management component.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,7 @@
 import { Routes } from '@angular/router';
 import {HomeComponent} from "./public/pages/home/home.component";
 import {AboutComponent} from "./public/pages/about/about.component";
-//import {CourseManagementComponent} from "./learning/pages/course-management/course-management.component";
+import {CourseManagementComponent} from "./learning/pages/course-management/course-management.component";
 import {PageNotFoundComponent} from "./public/pages/page-not-found/page-not-found.component";
 import {PersonComponent} from "./public/pages/person/person.component";
 
@@ -9,7 +9,7 @@ export const routes: Routes = [
   { path: 'home',             component: HomeComponent },
   { path: 'about',            component: AboutComponent },
   {path:'person',             component: PersonComponent},
-  //{ path: 'learning/courses', component: CourseManagementComponent},
+  { path: 'learning/courses', component: CourseManagementComponent},
   { path: '',                 redirectTo: 'home', pathMatch: 'full' },
   { path: '**',               component: PageNotFoundComponent },
 
