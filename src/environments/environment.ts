a82a91d4309fd9e2f3286b930c83234e4b2ae78b
XaerickSReborn <u202213358@upc.edu.pe>
2024-09-19 19:30:36 -0500

    chore: configure environment variables support.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,7 +1,4 @@
 export const environment = {
-    production: false,
-    // Server Base Path for Fake REST API
-    serverBasePath: 'http://localhost:3000/api/v1'
-    // Server Base Path for Spring Boot REST API
-    //serverBasePath: 'http://localhost:8080/api/v1'
-  };
+  production: true,
+  serverBasePath: 'https://my-json-server.typicode.com/efrainbautista/upc-pre-si729'
+};
